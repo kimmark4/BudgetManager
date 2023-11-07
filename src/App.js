ae510138f853a30085d4fa71bc44471235c9c667
Mark <mark.kimk622@gmail.com>
2023-11-06 21:29:15 -0500

    starting files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// Modules
+import { useState } from 'react';
+// Styling
 import './App.css';
+// Components
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Budget Tracker</h1>
     </div>
   );
 }
