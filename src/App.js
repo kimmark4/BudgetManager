35381ac8a9fcaf629ad0f54766c441f2dfd0a0bb
Mark <mark.kimk622@gmail.com>
2023-11-07 21:35:39 -0500

    form html and basic functionality

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import { useState } from 'react';
 // Styling
 import './App.css';
 // Components
-
-
+import Header from './components/Header';
+import AddMoney from './components/AddMoney';
+import ListOfMoney from './components/ListOfMoney.js';
 
 function App() {
+
+  const [expenseList] = useState([]);
+
+
+  const addExpense = (e, expense) => {
+    e.preventDefault();
+    console.log(expense);
+    expenseList.push(expense);
+    console.log(expenseList);
+  }
+
+
   return (
     <div className="App">
-      <h1>Budget Tracker</h1>
+      <Header />
+      <AddMoney addExpense={addExpense} />
+      <form action="" ><button type='submit'>Log app.js</button></form>
+      <ListOfMoney expenseList={expenseList}/> 
     </div>
   );
 }
---
+++ src/components/ListOfMoney.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+
+const ListOfMoney = ({expenseList}) => {
+
+    const [list, setList] = useState(expenseList);
+    
+    return (
+        <div>
+            <ul className="expenseList">
+                {list == []
+                    ?
+                    list.map((expense) => {
+                        return (
+                            <li>{expense}</li>
+                        )
+                    })
+                    :
+                    <h2>No Expenses</h2>
+                }
+            </ul>
+            <ul className="incomeList"></ul>
+        </div>
+    )
+}
+
+export default ListOfMoney;
